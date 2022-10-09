d5b620a6221ee6d099b2c333c23c729fc4ed3cc9
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-10-09 19:59:03 +0300

    commit tree 9a0a4084619ef3dab8f7da29e82465f3cc8f1a41

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,6 +1,9 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
+      t.string :name
+      t.integer :capacity
+      t.string :colour
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Passenger.create(name: "Oscar Abuto", price:12300)
